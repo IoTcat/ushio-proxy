b98e77e641a3c896237c00e3e65374bcba9aa1ae
cn.yimian.xyz <i@iotcat.me>
2020-07-09 11:24:34 +0800

    docker

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const request = require('request');
+const app = express()
+const port = 3000
+
+app.listen(port, () => console.log(`Ushio-proxy listening at http://localhost:${port}`))
+app.use(express.static(__dirname + '/public'));
+
+app.get('/get/', (req, res) => {
+
+    if(req.query.hasOwnProperty('url')){
+        request(new Buffer(req.query.url, 'base64').toString()).pipe(res);
+    }
+
+})
